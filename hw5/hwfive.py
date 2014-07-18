9676a6a41049923e6c375984f12659f94df37fe6
unknown <rwstep7017@GIS-VLAB3.ad.uauth.net>
2014-07-18 15:56:16 -0400

    updating finished homework 5.

--- hw5/hwfive.py
+++ hw5/hwfive.py
@@ -1,7 +1,15 @@
 import arcpy
 from arcpy import env
-arcpy.env.workplace = path
+arcpy.env.workplace = "P:/ESRIPress/Python/Data/Exercise06"
 fclist = arcpy.ListFeatuerClasses()
 
 for feature in fclist:
-    myshape = arcpy.Describe(fclist)
+    myshape = arcpy.Describe(feature)
+    print "{} is a {} {}".format(myshape.name, myshape.shapeType, myshape.datasetType)
+
+arcpy.createFileGDB_management("P:/ESRIPress/Python/Data/Exercise06/Results", "NM.gdb")
+for fc in fclist:
+    shape = arcpy.Describe (fc)
+    if shape.shapeType == u'polygon':
+        arcpy.CopyFeatures_management(fc, "P:/ESRIPress/Python/Data/Exercise06/Results/NM.gbd/" + shape.basename)
+        
